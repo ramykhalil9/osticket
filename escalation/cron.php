811fca1339c1c92f0c5781cebe15a9de64a472d5
Andy Abi Haidar <andy@xtnd.io>
2019-01-24 17:04:23 +0200

    Implemented PHPMailer

--- escalation/cron.php
+++ escalation/cron.php
@@ -10,32 +10,6 @@ use PHPMailer\PHPMailer\Exception;
 // Importing mail class
 require __DIR__ . "/vendor/autoload.php";
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                    // Set mailer to use SMTP
-    $mail->Host = 'lbfcmail.bmbgroup.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    // $mail->Username = 'user@example.com';                 // SMTP username
-    // $mail->Password = 'secret';                           // SMTP password
-    // $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 25;                                    // TCP port to connect to
-
-    //Recipients
-    $mail->setFrom('tac@bmbgroup.com');
-    $mail->addAddress('andy@yllw.com');     // Add a recipient
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    die($mail->send());
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-    die;
-}
 
 // Fetching all the tickets that are open
 // Closed means status_id = 3, so checking for anything different than 3
@@ -210,36 +184,43 @@ function incrementReminder($db, $i, $id) {
 }
 
 function sendMail($ticketID, $openFor, $tm = false, $sm = false, $om = false, $president = false) {
-    $email = new \SendGrid\Mail\Mail(); 
-    $email->setFrom("andy@yllw.com");
-    $email->setSubject("Ticket #{$ticketID} is still open.");
-
-    if($tm) {
-        $email->addTo("andy@yllw.com");
-    }
-
-    if($sm) {
-        $email->addTo("andy.abihaidar@xtnd.io");
-    }
-
-    if($om) {
-        $email->addTo("andy@xtnd.io");
-    }
-
-    if($president) {
-        $email->addTo("andyabihaidar@gmail.com");
-    }
-
-    $email->addContent("text/plain", "Please note ticket #{$ticketID} has been open for {$openFor} hours.");
-    $email->addContent(
-        "text/html", "Please note ticket #{$ticketID} has been open for {$openFor} hours."
-    );
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
     try {
-        $response = $sendgrid->send($email);
-        print $response->statusCode() . "\n";
-        print_r($response->headers());
-        print $response->body() . "\n";
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                    // Set mailer to use SMTP
+        $mail->Host = 'lbfcmail.bmbgroup.com';  // Specify main and backup SMTP servers
+        $mail->Port = 25;                                    // TCP port to connect to
+        $mail->SMTPSecure = false;
+        $mail->SMTPAutoTLS = false;
+
+        //Recipients
+        $mail->setFrom('tac@bmbgroup.com');
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = "Ticket #{$ticketID} is still open.";
+        $mail->Body    = "Please note ticket #{$ticketID} has been open for {$openFor} hours.";
+        $mail->AltBody = "Please note ticket #{$ticketID} has been open for {$openFor} hours.";
+            
+        if($tm) {
+            $mail->addAddress("andy@yllw.com");
+        }
+
+        if($sm) {
+            $mail->addAddress("andy.abihaidar@xtnd.io");
+        }
+
+        if($om) {
+            $mail->addAddress("andy@xtnd.io");
+        }
+
+        if($president) {
+            $mail->addAddress("andyabihaidar@gmail.com");
+        }
+
+        $mail->send();
+        echo "Message has been sent for Ticket ID {$ticketID}";
     } catch (Exception $e) {
-        echo 'Caught exception: '. $e->getMessage() ."\n";
+        echo "Message could not be sent for Ticket ID {$ticketID} Mailer Error: ", $mail->ErrorInfo;
     }
+
 }
